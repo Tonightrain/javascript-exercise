aa9aa43000e4fdea9a7d6b60973986df99d6c748
xiaomeiqi <805560812@qq.com>
2020-08-10 22:20:04 +0800

    1 2

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,14 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  return collection.filter(i => i % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  // eslint-disable-next-line func-names
+  return collection.filter(function(element, index, self) {
+    return self.indexOf(element) === index;
+  });
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
