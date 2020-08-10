a94c2e7771a677d371a3c3d4debeef541a443f7d
xiaomeiqi <805560812@qq.com>
2020-08-11 01:17:00 +0800

    async

--- 5-async/1-xhr/index.js
+++ 5-async/1-xhr/index.js
@@ -3,16 +3,25 @@ function fetchData(url, successCallback, errorCallback) {
   // <-- start
   // TODO 21: 通过XMLHttpRequest实现异步请求
 
+  xhr.open('GET', url);
+  // eslint-disable-next-line func-names
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        successCallback(xhr);
+      } else {
+        errorCallback(xhr);
+      }
+    }
+  };
+  xhr.send();
   // end -->
 }
-
+function handeleSuccess(xhr) {
+  console.log(xhr.responseText);
+}
+function handleError(xhr) {
+  console.log(xhr.statusText);
+}
 const URL = 'http://localhost:3000/api';
-fetchData(
-  URL,
-  result => {
-    document.writeln(JSON.parse(result).name);
-  },
-  error => {
-    console.error(error);
-  }
-);
+fetchData(URL, handeleSuccess, handleError);
